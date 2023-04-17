823e593cc98114dd3dec3c837af9dc6851e1283b
Jack Sprinkle <sprinkle.jas@gmail.com>
2023-04-17 14:39:09 -0400

    install database dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import Head from 'next/head'
 
-
-
-
-export default function Home() {
+export default function LoginPage() {
   return (
     <>
       <Head>
-        <title>Book Clube Helper</title>
+        <title>Book Club Helper</title>
         <meta name="Book Club Helper" content="An application to help with book clubs!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
