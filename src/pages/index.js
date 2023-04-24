bc60a3efe6894ff37f37d0c26e0ae4bf9ab15c6c
Jack Sprinkle <sprinkle.jas@gmail.com>
2023-04-24 15:14:14 -0400

    finish mobile styling for login/register

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,22 @@ import Register from "@/components/Register";
 import Login from "@/components/Login";
 import styles from "../styles/pages/MainPage.module.scss";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function MainPage() {
   const [existingUser, setExistingUser] = useState(false);
 
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    if (!token) {
+      setExistingUser(false);
+    } else {
+      setExistingUser(true);
+    }
+  }, []);
+
   const handleClick = () => {
-    setExistingUser(true);
+    setExistingUser(!existingUser);
   };
   if (!existingUser) {
     return (
@@ -23,23 +32,43 @@ export default function MainPage() {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           {/* <link rel="icon" href="/favicon.ico" /> */}
         </Head>
-        <main className={styles.main__container}>
+        <main className={styles.main}>
           <Register />
-          <div>
-            <p>Already a nerd?</p> 
-            <button onClick={handleClick}>Sign in</button>
-
+          <div className={styles.main__subcontainer}>
+            <p className={styles.main__subheading}>Already a nerd?</p>
+            <button className={styles.main__button} onClick={handleClick}>
+              Sign in
+            </button>
           </div>
         </main>
       </>
     );
   }
 
-  if(existingUser) {
+  if (existingUser) {
     return (
       <>
-      <Login />
+        <Head>
+          <title>Book Club Helper</title>
+          <meta
+            name="Book Club Helper"
+            content="An application to help with book clubs!"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {/* <link rel="icon" href="/favicon.ico" /> */}
+        </Head>
+        <main className={styles.main}>
+          <Login />
+          <div className={styles.main__subcontainer}>
+            <p className={styles.main__subheading}>
+              Need to create an account?
+            </p>
+            <button className={styles.main__button} onClick={handleClick}>
+              Sign Up
+            </button>
+          </div>
+        </main>
       </>
-    )
+    );
   }
 }
