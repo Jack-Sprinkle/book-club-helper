1ef5fed1535d13c70c0dd98612b16b6dd6e7a901
Jack Sprinkle <sprinkle.jas@gmail.com>
2023-05-02 15:39:02 -0400

    create api route for selected book

---
+++ src/pages/api/books/single.js
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+import nextConnect from "next-connect";
+import authMiddleware from '../middleware/authorize';
+
+const singleBookRoute = nextConnect({});
+
+singleBookRoute.use(authMiddleware);
+
+singleBookRoute.get(async (req, res) => {
+    const prisma = new PrismaClient();
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth()
+
+    try {
+        const selectedBook = await prisma.books.findMany({
+            where: {
+                monthRecommended: currentMonth
+            },
+
+            orderBy: [
+                {
+                    votes: 'desc'
+                }
+            ],
+
+            take: 1,
+
+            select: {
+                id: true,
+                title: true,
+                author: true,
+                description: true,
+                userRatings: true,
+                votes: true,
+
+            }
+        })
+
+
+
+        return res.status(200).json(selectedBook)
+    } catch {
+        return res.status(400).send("Failed to find selected book")
+    }
+})
+
+export default singleBookRoute;
