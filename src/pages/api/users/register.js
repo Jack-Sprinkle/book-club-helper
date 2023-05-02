5af56e2638e95435787cd972ffe114d90683d3ab
Jack Sprinkle <sprinkle.jas@gmail.com>
2023-05-02 13:18:47 -0400

    refactor register route using next-connect

--- src/pages/api/users/register.js
+++ src/pages/api/users/register.js
@@ -1,40 +1,45 @@
 import { PrismaClient } from "@prisma/client";
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
+import nextConnect from "next-connect";
 
-export default async function handler(req, res) {
-  const prisma = new PrismaClient();
-
-  if (req.method === "POST") {
-    //desctucture request body
-    const { user_name, password } = req.body;
-
-    //make sure there is a user name and password in the request body
-    if (!user_name || !password) {
-      return res.status(400).send("Please enter the required fields");
-    }
-
-    //Hash and salt the user password before storing in the database
-    const salt = bcrypt.genSaltSync(2);
-    const hashedPassword = bcrypt.hashSync(password, salt);
-
-    //create the user in the database
-
-    try {
-       await prisma.users.create({
-        data: {
-          id: uuidv4(),
-          userName: user_name,
-          password: hashedPassword,
-        },
-      });
-
-      return res.status(201).send("Registered Successfully");
-    } catch {
-      return res.status(400).send("Failed to Register")
-    }
-    
-
-    
+const register = nextConnect({
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` })
   }
-}
+})
+
+register.post(async (req, res) => {
+  const prisma = new PrismaClient();
+  //desctucture request body
+  const { user_name, password } = req.body;
+
+  //make sure there is a user name and password in the request body
+  if (!user_name || !password) {
+    return res.status(400).send("Please enter the required fields");
+  }
+
+  //Hash and salt the user password before storing in the database
+  const salt = bcrypt.genSaltSync(2);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  //create the user in the database
+
+  try {
+    await prisma.users.create({
+      data: {
+        id: uuidv4(),
+        userName: user_name,
+        password: hashedPassword,
+      },
+    });
+
+    return res.status(201).send("Registered Successfully");
+  } catch {
+    return res.status(400).send("Failed to Register")
+  }
+});
+
+export default register
+
+
